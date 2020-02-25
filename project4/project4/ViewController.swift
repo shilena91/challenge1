b4f511d511568f5abf4f7a46d42fc8eb5d8f0d99
HoangPham <pham.hoang1591@gmail.com>
2020-02-25 18:26:03 +0200

    add project 4 unfinished

---
+++ project4/project4/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  project4
+//
+//  Created by Hoang Pham on 2/25/20.
+//  Copyright © 2020 Hoang Pham. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+
+
+}
+
